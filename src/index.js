9daf49adf0ba9e4faf635800c5ba53286a6547b9
Bobyoung0719 <Bobyoung0719@gmail.com>
2020-06-11 18:20:44 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
+const CLUI = require('clui');
 const chalk = require('chalk');
 const clear = require('clear');
 const figlet = require('figlet');
 
-// 文件处理
-const files = require('../lib/files');
+const {scaffoldPool} = require('../lib/params');
+const {directoryExists} = require('../lib/files');
+const {gitCloneStart} = require('../lib/clone');
+const {askGithubCredentials} = require('../lib/inquirer');
+
+const spin = (t1, t2 = '') => new CLUI.Spinner(t1, t2);
 
 clear();
 
@@ -15,4 +20,33 @@ console.log(
 );
 
 // 开始生成
-console.log('----- start program -----');
+console.log(chalk.yellow('<----- Let`s start a new project ----->'));
+
+// 用户交互式命令
+const run = async () => {
+  try {
+    const {
+      isSureRes,
+      projectName,
+      projectAuthor, 
+      projectScaffold, 
+      projectDescription
+    } = await askGithubCredentials();
+
+    if (!isSureRes) {
+      console.log(chalk.yellow(`<----- you have choose 'No' to end!!! ----->`));
+
+      process.exit();
+    }
+
+    const repoPath = scaffoldPool[projectScaffold];
+    spin('git clone start, please waitting', ['⣾','⣽','⣻','⢿','⡿','⣟','⣯','⣷']).start();
+    await gitCloneStart(repoPath, projectName);
+    spin('git clone finished').stop();
+  } catch (error) {
+    console.log(error)
+  }
+
+} 
+
+run();
