d67bc91a09820a9ececcb016834b6b1fc64eabe3
Bobyoung0719 <Bobyoung0719@gmail.com>
2020-06-11 19:07:29 +0800

    update

--- src/index.js
+++ src/index.js
@@ -5,19 +5,16 @@ const figlet = require('figlet');
 
 const {scaffoldPool} = require('../lib/params');
 const {gitCloneStart} = require('../lib/clone');
+const {editPackJson} = require('../lib/files');
 const {askGithubCredentials} = require('../lib/inquirer');
-const {directoryExists, editPackJson} = require('../lib/files');
+
 
 const spin = (t1, t2 = '') => new CLUI.Spinner(t1, t2);
 
 clear();
 
-// 先来画一个牛逼的Mech
-console.log(
-  chalk.yellow(
-    figlet.textSync('MECH CLI', {horizontalLayout: 'full'})
-  )
-);
+// 先来画一个牛逼的图案
+console.log(chalk.yellow(figlet.textSync('MECH CLI', {horizontalLayout: 'full'})));
 
 // 开始生成
 console.log(chalk.yellow('<----- Let`s start a new project ----->'));
@@ -25,6 +22,7 @@ console.log(chalk.yellow('<----- Let`s start a new project ----->'));
 // 用户交互式命令
 const run = async () => {
   try {
+    // 获取用户输入信息
     const {
       isSureRes,
       projectName,
@@ -39,19 +37,20 @@ const run = async () => {
       process.exit();
     }
 
+    // 仓库地址
     const repoPath = scaffoldPool[projectScaffold];
     // spin('git clone start, please waitting', ['⣾','⣽','⣻','⢿','⡿','⣟','⣯','⣷']).start();
+
+    // 克隆仓库
     await gitCloneStart(repoPath, projectName);
     // spin('git clone finished').stop();
 
     // 重新编辑package.json 
-
-    editPackJson(projectName, projectAuthor, projectDescription)
+    editPackJson(projectName, projectAuthor, projectDescription);
 
   } catch (error) {
     console.log(error)
   }
-
 } 
 
 run();
